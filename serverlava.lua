038733a1dd8bfcf361a90032ef24f220340c2ae4
tsubugumi0929 <141685560+mame1123ccc@users.noreply.github.com>
2025-03-16 17:02:56 +0900

    Create serverlava.lua

---
+++ serverlava.lua
@@ -0,0 +1,19 @@
+-- サーバー側スクリプト
+
+local lavaParts = workspace.Map.GhostShipInterior.LavaParts -- LavaPartsの親
+
+-- Lavaパーツを削除する関数
+local function destroyLava()
+    while true do
+        -- Lavaパーツを取得して削除
+        for _, lavaPart in pairs(lavaParts:GetChildren()) do
+            if lavaPart.Name == "Lava" then
+                lavaPart:Destroy()
+            end
+        end
+        wait(0.01) -- 0.01秒ごとに削除
+    end
+end
+
+-- Lava削除を開始
+destroyLava()
