783b6a608c4066ad0672ecf903e1c98d677bcf5f
tsubugumi0929 <141685560+mame1123ccc@users.noreply.github.com>
2025-03-16 20:51:11 +0900

    Delete locallava2.lua

--- locallava2.lua
+++
@@ -1,8 +0,0 @@
-while true do
-    for _, part in pairs(game.Workspace:GetDescendants()) do
-        if part:IsA("BasePart") and part.Name == "Lava" then
-            part:Destroy()
-        end
-    end
-    wait(0.1) -- 0.1秒ごとにチェック
-end
