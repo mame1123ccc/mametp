e03471bc86bd835c10b2dc6a7a8bb8b7b91ff2cc
tsubugumi0929 <141685560+mame1123ccc@users.noreply.github.com>
2025-03-16 17:09:55 +0900

    Create ghostmame.lua

---
+++ ghostmame.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/mame1123ccc/mametp/refs/heads/main/ghostrule.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/mame1123ccc/mametp/refs/heads/main/local%20lava.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/mame1123ccc/mametp/refs/heads/main/serverlava.lua"))()
